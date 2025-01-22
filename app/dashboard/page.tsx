d4d8b2e71b824b838cf81d568f186e5881fbe9ab
SaukiFutaki <utaki.sauki19@gmail.com>
2025-01-22 22:09:20 +0700

    feat(navbar) : add navbar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,11 @@
-import { Calendar } from '@/components/Test'
+
 import React from 'react'
 
 export default function Page() {
   return (
     <div>
       
-        <Calendar />
+     
     </div>
   )
 }
