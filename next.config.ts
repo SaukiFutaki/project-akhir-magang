2d551778d6419326c164806430271bb745168d31
SaukiFutaki <utaki.sauki19@gmail.com>
2025-02-09 05:57:35 +0700

    t

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ 
   experimental: {
     optimizePackageImports: ["@heroicons/react", "react-icons"],
     serverActions :{
@@ -14,8 +15,21 @@ const nextConfig: NextConfig = {
         hostname: 'dyapehwgtewrhepdzifu.supabase.co',
       }
     ]
-  }
-  
+  },
+  async headers() {
+    return [
+      {
+        // Mengizinkan untuk semua API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ]
+      }
+    ];
+  }  
 };
 
 export default nextConfig;
