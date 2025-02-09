15ca06cf653a6a94937dd65eab6735cb102e8e03
SaukiFutaki <utaki.sauki19@gmail.com>
2025-02-09 09:16:59 +0700

    feat(ncon) : hidden payload

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- 
   experimental: {
     optimizePackageImports: ["@heroicons/react", "react-icons"],
-    serverActions :{
-      bodySizeLimit : "100mb",
-    }
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
   },
-  images : {
-    remotePatterns :[
+  images: {
+    remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'dyapehwgtewrhepdzifu.supabase.co',
-      }
-    ]
+        protocol: "https",
+        hostname: "dyapehwgtewrhepdzifu.supabase.co",
+      },
+    ],
   },
   async headers() {
     return [
       {
-        // Mengizinkan untuk semua API routes
-        source: '/api/:path*',
+      
+        source: "/api/:path*",
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ]
-      }
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
     ];
-  }  
+  },
 };
 
 export default nextConfig;
